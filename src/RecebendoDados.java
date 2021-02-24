11b8a647f937a2cd4f511a8eb5027f7e6a0f2136
Jamille <jamille.pm93@gmail.com>
2021-02-24 20:46:09 -0300

    Final da Aula01

---
+++ src/RecebendoDados.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class RecebendoDados {
+
+	public static void main(String[] args) {
+		// A linha abaixo cria um Scanner. Um scanner é usado para lermos os dados que o usuário digitar pelo teclado
+			
+		Scanner leitor = new Scanner(System.in);
+		
+		//A linha abaixo cria uma variável (espaço na memória) para guardarmos um dado. Nesse caso, o nome do usuário.
+		String nomeUsuario;
+		
+		System.out.println("Por favor, escreva seu nome:");
+		
+		// A linha abaixo lê o texto que o usuário escrever no seu teclado
+		nomeUsuario = leitor.next();
+		
+		//A linha abaixo exibe o conteúdo da variável. Ela faz isso ao exibir e juntá-lo à variável
+		System.out.println("Obrigada por utilizar o programa, " + nomeUsuario);
+		
+		//Sempre precisamos liberar o scanner após o uso
+		leitor.close();
+		
+		
+
+	}
+
+}
